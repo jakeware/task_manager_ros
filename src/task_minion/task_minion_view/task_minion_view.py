40d7027e61dafebef06857eb9835a3a45a472965
Jake Ware <jakeware@gmail.com>
2019-01-02 00:20:48 -0500

    Add header entry.

--- src/task_minion/task_minion_view/task_minion_view.py
+++ src/task_minion/task_minion_view/task_minion_view.py
@@ -3,6 +3,36 @@
 from Tkinter import *
 from ScrolledText import ScrolledText
 
+class HeaderEntry:
+    def __init__(self, parent_frame):
+        self.master_frame = Frame(parent_frame)
+        self.master_frame.pack(fill=X)
+
+        self.name_text = Text(self.master_frame, height=1, width=10, highlightthickness=0, borderwidth=1, wrap='none')
+        self.name_text.pack(side=LEFT)
+        self.name_text.insert('1.0', 'Name')
+        self.name_text.config(state='disabled')
+        
+        self.status_text = Text(self.master_frame, height=1, width=10, highlightthickness=0, borderwidth=1, wrap='none')
+        self.status_text.pack(side=LEFT)
+        self.status_text.insert('1.0', 'Status')
+        self.status_text.config(state='disabled')
+        
+        self.load_text = Text(self.master_frame, height=1, width=10, highlightthickness=0, borderwidth=1, wrap='none')
+        self.load_text.pack(side=LEFT)
+        self.load_text.insert('1.0', 'Load')
+        self.load_text.config(state='disabled')
+        
+        self.memory_text = Text(self.master_frame, height=1, width=10, highlightthickness=0, borderwidth=1, wrap='none')
+        self.memory_text.pack(side=LEFT)
+        self.memory_text.insert('1.0', 'Memory')
+        self.memory_text.config(state='disabled')
+        
+        self.message_text = Text(self.master_frame, height=1, highlightthickness=0, borderwidth=1, wrap='none')
+        self.message_text.pack(side=LEFT,fill=X, expand=1)
+        self.message_text.insert('1.0', 'Message')
+        self.message_text.config(state='disabled')
+
 class TaskEntry:
     def __init__(self, parent_frame, task_id, task_name, task_depth):
         self.task_id = task_id
@@ -55,6 +85,7 @@ class TaskMinionView:
         self.root.title("Task Master")
         self.task_entries = {}  # dictionary (indexed by task_id) of TaskEntries
         self.task_order = []  # list of task ids in order they are displayed
+        self.header = HeaderEntry(self.root)
         self.task_frame = Frame(self.root)
         self.task_frame.pack(fill=X)
         self.output_text = ScrolledText(self.root)
