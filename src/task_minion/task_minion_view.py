cfa28fae0e4673240227a48cd491be109b72685c
Jake Ware <jakeware@gmail.com>
2019-01-04 04:02:34 -0500

    Work on colors and cleanup.

--- src/task_minion/task_minion_view.py
+++ src/task_minion/task_minion_view.py
@@ -7,36 +7,38 @@ class HeaderEntry(object):
     def __init__(self, parent_frame):
         self.master_frame = Frame(parent_frame)
         self.master_frame.grid(row=0, column=0, sticky=NSEW)
+        self.bg_color = 'navy'
+        self.fg_color = 'white'
 
-        self.name_text = Text(self.master_frame, height=1, width=10, highlightthickness=0, borderwidth=1, wrap='none')
+        self.name_text = Text(self.master_frame, height=1, width=10, highlightthickness=0, borderwidth=0, wrap='none', bg=self.bg_color, fg=self.fg_color)
         self.name_text.pack(side=LEFT)
         self.name_text.tag_configure("center", justify='center')
         self.name_text.insert('1.0', 'Name')
         self.name_text.tag_add("center", "1.0", END)
         self.name_text.config(state='disabled')
         
-        self.status_text = Text(self.master_frame, height=1, width=10, highlightthickness=0, borderwidth=1, wrap='none')
+        self.status_text = Text(self.master_frame, height=1, width=10, highlightthickness=0, borderwidth=0, wrap='none', bg=self.bg_color, fg=self.fg_color)
         self.status_text.pack(side=LEFT)
         self.status_text.tag_configure("center", justify='center')
         self.status_text.insert('1.0', 'Status')
         self.status_text.tag_add("center", "1.0", END)
         self.status_text.config(state='disabled')
         
-        self.load_text = Text(self.master_frame, height=1, width=10, highlightthickness=0, borderwidth=1, wrap='none')
+        self.load_text = Text(self.master_frame, height=1, width=10, highlightthickness=0, borderwidth=0, wrap='none', bg=self.bg_color, fg=self.fg_color)
         self.load_text.pack(side=LEFT)
         self.load_text.tag_configure("center", justify='center')
         self.load_text.insert('1.0', 'Load')
         self.load_text.tag_add("center", "1.0", END)
         self.load_text.config(state='disabled')
         
-        self.memory_text = Text(self.master_frame, height=1, width=10, highlightthickness=0, borderwidth=1, wrap='none')
+        self.memory_text = Text(self.master_frame, height=1, width=10, highlightthickness=0, borderwidth=0, wrap='none', bg=self.bg_color, fg=self.fg_color)
         self.memory_text.pack(side=LEFT)
         self.memory_text.tag_configure("center", justify='center')
         self.memory_text.insert('1.0', 'Memory')
         self.memory_text.tag_add("center", "1.0", END)
         self.memory_text.config(state='disabled')
         
-        self.message_text = Text(self.master_frame, height=1, highlightthickness=0, borderwidth=1, wrap='none')
+        self.message_text = Text(self.master_frame, height=1, highlightthickness=0, borderwidth=0, wrap='none', bg=self.bg_color, fg=self.fg_color)
         self.message_text.pack(side=LEFT,fill=X, expand=1)
         self.message_text.tag_configure("center", justify='center')
         self.message_text.insert('1.0', 'Message')
@@ -49,65 +51,65 @@ class TaskEntry(object):
         
         self.master_frame = Frame(parent_frame)
         self.master_frame.pack(fill=X)
+        self.fg_color = 'white'
+        self.bg_color_inactive = 'black'
+        self.bg_color_active = 'gray'
 
         depth_indent = task_depth * '  '
-        self.name_text = Text(self.master_frame, height=1, width=10, highlightthickness=0, borderwidth=1, wrap='none')
+        self.name_text = Text(self.master_frame, height=1, width=10, highlightthickness=0, borderwidth=0, wrap='none', bg='black', fg=self.fg_color)
         self.name_text.pack(side=LEFT)
         self.name_text.insert('1.0', depth_indent + task_name)
         self.name_text.config(state='disabled')
         
-        self.status_text = Text(self.master_frame, state='disabled', height=1, width=10, highlightthickness=0, borderwidth=1, wrap='none')
+        self.status_text = Text(self.master_frame, state='disabled', height=1, width=10, highlightthickness=0, borderwidth=0, wrap='none', bg='black', fg=self.fg_color)
         self.status_text.pack(side=LEFT)
         
-        self.load_text = Text(self.master_frame, state='disabled', height=1, width=10, highlightthickness=0, borderwidth=1, wrap='none')
+        self.load_text = Text(self.master_frame, state='disabled', height=1, width=10, highlightthickness=0, borderwidth=0, wrap='none', bg='black', fg=self.fg_color)
         self.load_text.pack(side=LEFT)
         
-        self.memory_text = Text(self.master_frame, state='disabled', height=1, width=10, highlightthickness=0, borderwidth=1, wrap='none')
+        self.memory_text = Text(self.master_frame, state='disabled', height=1, width=10, highlightthickness=0, borderwidth=0, wrap='none', bg='black', fg=self.fg_color)
         self.memory_text.pack(side=LEFT)
         
-        self.message_text = Text(self.master_frame, state='disabled', height=1, highlightthickness=0, borderwidth=1, wrap='none')
+        self.message_text = Text(self.master_frame, state='disabled', height=1, highlightthickness=0, borderwidth=0, wrap='none', bg='black', fg=self.fg_color)
         self.message_text.pack(side=LEFT,fill=X, expand=1)
-
-        self.task_background_color_inactive = 'white'
-        self.task_background_color_active = 'blue'
 
         self.SetInactive()        
 
     def SetActive(self):
-        color = self.task_background_color_active
-        self.name_text['bg'] = color
-        self.status_text['bg'] = color
-        self.load_text['bg'] = color
-        self.memory_text['bg'] = color
-        self.message_text['bg'] = color
+        self.name_text['bg'] = self.bg_color_active
+        self.status_text['bg'] = self.bg_color_active
+        self.load_text['bg'] = self.bg_color_active
+        self.memory_text['bg'] = self.bg_color_active
+        self.message_text['bg'] = self.bg_color_active
 
     def SetInactive(self):
-        color = self.task_background_color_inactive
-        self.name_text['bg'] = color
-        self.status_text['bg'] = color
-        self.load_text['bg'] = color
-        self.memory_text['bg'] = color
-        self.message_text['bg'] = color
+        self.name_text['bg'] = self.bg_color_inactive
+        self.status_text['bg'] = self.bg_color_inactive
+        self.load_text['bg'] = self.bg_color_inactive
+        self.memory_text['bg'] = self.bg_color_inactive
+        self.message_text['bg'] = self.bg_color_inactive
 
     def SetTaskLoad(self, task_load):
         self.load_text.config(state='normal')
-        self.load_text.insert('1.0', str(task_load))
+        self.load_text.insert('1.0', str(round(task_load,2)))
         self.load_text.config(state='disabled')
 
     def SetTaskMemory(self, task_memory):
         self.memory_text.config(state='normal')
-        self.memory_text.insert('1.0', str(task_memory))
+        self.memory_text.insert('1.0', str(round(task_memory,2)))
         self.memory_text.config(state='disabled')
 
 class OutputEntry(object):
     def __init__(self, parent_frame, task_id):
         self.task_id = task_id
+        self.bg_color = 'black'
+        self.fg_color = 'white'
 
         self.master_frame = Frame(parent_frame)
         self.master_frame.grid(row=2, column=0, sticky=NSEW)
         self.master_frame.grid_rowconfigure(0, weight=1)
         self.master_frame.grid_columnconfigure(0, weight=1)
-        self.output_text = ScrolledText(self.master_frame)
+        self.output_text = ScrolledText(self.master_frame, bg=self.bg_color, fg=self.fg_color)
         self.output_text.grid(row=0, column=0, sticky=NSEW)
         self.output_text.insert('1.0', "Task ID:" + str(task_id))
         self.output_text.config(state='disabled')
@@ -137,7 +139,7 @@ class TaskMinionView(object):
 
         # layout
         self.root = root
-        self.root.title("Task Master")
+        self.root.title("Task Minion")
         self.root.grid_rowconfigure(2, weight=1)
         self.root.grid_columnconfigure(0, weight=1)
         self.header = HeaderEntry(self.root)
